003ec947c7e8c5e23be55ccafa5a6267e74b0afc
jskim <ks78mk2@naver.com>
2021-07-29 17:13:14 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, Suspense , useEffect, useState } from 'react'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import GlobalContextProvider, { useGlobalContext } from './contexts/GlobalContext';
+import AccountsRouter from './routers/AccountsRouter';
+import ContentsRouter from './routers/ContentsRouter';
+
+const RoutercontentView = () => {
+    return (
+        <>
+            <Router>
+                <Suspense fallback={null}>
+                    <Switch>
+                        <Route exact path="/accounts*" component={AccountsRouter}/>
+                        <Route exact path="/contents*" component={ContentsRouter}/>
+                        <Route exact path="/" component={() => {
+                            return (
+                                <Redirect to="/accounts" />
+                            )
+                        }}/>
+                        <Route exact path="*" component={}/>
+                    </Switch>
+                </Suspense>
+            </Router>  
+        </>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>HELLO REACTzzz11zz!!</p>        
-      </header>
-    </div>
-  );
+    return (
+        <GlobalContextProvider>
+            <RoutercontentView/>
+        </GlobalContextProvider>    
+    );
 }
 
 export default App;
